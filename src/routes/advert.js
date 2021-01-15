d8885f4577bc4ef1dce5d520fd59d7da81db71df
小可爱 <2659580957@qq.com>
2021-01-15 09:28:59 +0800

    异步无刷新分页

--- src/routes/advert.js
+++ src/routes/advert.js
@@ -9,32 +9,45 @@ import { basename } from 'path';
 const router = express.Router();
 
 
+router.get('/advert/count/', (req, res, next) => {
+    Advert.count((err, count) => {
+        if (err) {
+            return next(err);
+        }
+        res.json({
+            err_code: 0,
+            result: count
+        });
+    })
+})
+
 //渲染advert_list首页
 router.get('/advert', (req, res, next) => {
-    const page = Number.parseInt(req.query.page, 10);
-    const pageSize = 5;
-    Advert
-        .find()
-        .skip((page - 1) * pageSize)
-        .limit(5)
-        .exec((err, adverts) => {
-            if (err) {
-                return next(err);
-            }
-            Advert.count((err, count) => {
-                if (err) {
-                    return next(err);
-                }
+    res.render('advert_list.ejs');
+    // const page = Number.parseInt(req.query.page, 10);
+    // const pageSize = 5;
+    // Advert
+    //     .find()
+    //     .skip((page - 1) * pageSize)
+    //     .limit(5)
+    //     .exec((err, adverts) => {
+    //         if (err) {
+    //             return next(err);
+    //         }
+    //         Advert.count((err, count) => {
+    //             if (err) {
+    //                 return next(err);
+    //             }
 
-                //总页码 = 总记录数 / 每页显示的大小
-                const totalPage = Math.ceil(count / pageSize + 1);
-                res.render('advert_list.ejs', {
-                    adverts,
-                    totalPage,
-                    page
-                });
-            })
-        })
+    //             //总页码 = 总记录数 / 每页显示的大小
+    //             const totalPage = Math.ceil(count / pageSize + 1);
+    //             res.render('advert_list.ejs', {
+    //                 adverts,
+    //                 totalPage,
+    //                 page
+    //             });
+    //         })
+    //     })
 });
 
 
@@ -56,49 +69,72 @@ router.post('/advert/add', (req, res, next) => {
     //fields就是接收到的表单中的普通数据字段
     //files就是表单中文件上传来的一些文件信息,例如文件大小,上传上来的文件路径..
 
-    form.parse(req, (err, fields, files) => {
-
-        if (err) {
-            return next(err);
-        }
-
-
-        const body = fields;
-
-        body.images = files.images.name;
-
-        //就是在body中添加一个image值就是图片上传上来的路径
-        const advert = new Advert({
-            title: body.title,
-            images: body.images,
-            link: body.link,
-            start_time: body.start_time,
-            end_time: body.end_time,
+    new Promise((res, rej) => {
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                rej(err);
+            }
+            res({ fields, files });
         });
+    })
 
-        advert.save((err, result) => {
+        .then(data => {
+            const body = data.fields;
+            body.images = data.files.images.name;
+
+            //就是在body中添加一个image值就是图片上传上来的路径
+            const advert = new Advert({
+                title: body.title,
+                images: body.images,
+                link: body.link,
+                start_time: body.start_time,
+                end_time: body.end_time,
+            });
+
+            return advert;
+        })
+        .then(data => {
+            data.save((err, result) => {
+                if (err) {
+                    return next(err);
+                }
+                res.json({
+                    err_code: 0
+                })
+            });
+        });
+});
+
+router.get('/advert/list', (req, res, next) => {
+    let { page, pageSize } = req.query
+    page = Number.parseInt(page);
+    pageSize = Number.parseInt(pageSize);
+
+    Advert
+        .find()
+        .skip((page - 1) * pageSize)
+        .limit(pageSize)
+        .exec((err, adverts) => {
             if (err) {
                 return next(err);
             }
             res.json({
-                err_code: 0
+                err_code: 0,
+                result: adverts
             })
-        });
-    });
-});
-
-router.get('/advert/list', (req, res, next) => {
-    Advert.find((err, date) => {
-        if (err) {
-            return next(err);
-        }
-
-        res.json({
-            err_code: 0,
-            result: date
         })
-    })
-    //res.end(nunjucks.render('advert_list.ejs'));
+
+    // Advert.find((err, date) => {
+    //     if (err) {
+    //         return next(err);
+    //     }
+
+    //     res.json({
+    //         err_code: 0,
+    //         result: date
+    //     })
+    // })
+    //res.render('advert_list.ejs');
 })
 
 // /advert/1
